bb9338099983e080dfc475e8507ec8702b75986b
Oscar <o.d.anota@gmail.com>
2017-01-23 23:09:41 -0600

    Práctica 2 Limites de las variables

---
+++ limites.c
@@ -0,0 +1,16 @@
+//Anota Olmos Oscar David
+//Practica 2
+
+#include <stdio.h>
+#include <limits.h>
+char *cad = "Hola mundo";
+
+int main(){
+  printf("%s\n",cad);
+  printf("%u\n", INT_MAX);
+  printf("%ld\n", LONG_MAX);
+  printf("%u\n", CHAR_MAX);
+  printf("%u\n", SHRT_MAX);
+  printf("%Ld\n", LLONG_MAX);
+  return 0;
+}
