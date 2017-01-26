55fe919cdeb06be49212f50f9b6f9e0d3cdba2db
MrNight1 <o.d.anota@gmail.com>
2017-01-26 15:51:58 -0600

    Práctica 14 - Read /dev/random

---
+++ practica14.c
@@ -0,0 +1,41 @@
+// Anota Olmos Oscar David
+// Practica 14 - Read /dev/random
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(int argc, char *argv[])
+{
+
+#if 0
+	if(argc<2)
+	{
+		perror("Se necesita un argumento\n");
+		return(1111);
+	}
+#endif
+
+	int fd; //Declaracion de un descriptor de archivos.
+	char *archivo="/dev/random\0";
+	int numero;
+
+	//fd=open(archivo,O_WRONLY|O_CREAT|O_APPEND,S_IRUSR|S_IWUSR);
+	//                                   set_readUser|set_writeUser
+	fd=open(archivo,O_RDONLY);
+	
+	if(fd==-1)
+		fprintf(stderr,"No se pudo abrir o crear el archivo");
+	printf("Descriptor de archivo: %d\n",fd);
+
+//#if 0
+	//if(write(fd,"hola123.,\n",10)==-1)
+	//	fprintf(stderr,"Error al escribir en el archivo.");
+	if(read(fd,&numero,4))
+	    printf("numero: %d\n",numero);
+//#endif
+  
+	if(close(fd)==-1)
+		fprintf(stderr,"No se pudo cerrar el descriptor de archivo");
+}
